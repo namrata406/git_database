24212b7e34fd8904ab19498bcf665a55f1e46f80
namrata <namrata4062021@gmail.com>
2025-08-06 14:24:25 +0530

    this is the sql database connectivity

---
+++ src/JDBCConnection/JDBC_Connection.java
@@ -0,0 +1,30 @@
+package JDBCConnection;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class JDBC_Connection {
+
+	public static void main(String[] args) throws SQLException {
+		// TODO Auto-generated method stub
+		String host = "localhost";
+		String port = "3306";
+		Connection con = DriverManager.getConnection("jdbc:mysql://"+host+":"+port+"/Employeeportal","root","Singh123@");
+		Statement stat = con.createStatement();
+		ResultSet rs = stat.executeQuery("Select * From EmployeeInfo Where age = 34");
+		while(rs.next()) {
+		System.out.print(rs.getString("name"));
+		System.out.print(rs.getString("id"));
+		System.out.println(rs.getString("dept"));
+		}
+
+
+		
+		
+
+	}
+
+}
